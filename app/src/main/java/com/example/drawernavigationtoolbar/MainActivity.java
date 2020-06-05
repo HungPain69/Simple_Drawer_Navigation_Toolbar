dc11d97f69ada3556262187b5474c9f0842a91bb
HungPain69 <hungpain69@gmail.com>
2020-06-05 22:01:21 +0700

    khoi tao cac doi tuong View va anh xa

--- app/src/main/java/com/example/drawernavigationtoolbar/MainActivity.java
+++ app/src/main/java/com/example/drawernavigationtoolbar/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.drawernavigationtoolbar;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class MainActivity extends AppCompatActivity implements  NavigationView.OnNavigationItemSelectedListener{
+    DrawerLayout mDrawerLayout;
+    NavigationView mNavigationView;
+    ActionBarDrawerToggle mToggle;
+    Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Init();
+    }
+
+    private void Init() {
+        mDrawerLayout = findViewById(R.id.drawer);
+        mNavigationView = findViewById(R.id.navigation_view);
+        mToolbar = findViewById(R.id.toolbar);
+
+        
+
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
     }
 }
